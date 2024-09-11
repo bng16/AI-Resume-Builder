6334a9a08a4b89b7f30e5efee293bc45affed2b2
bng16 <biswajitnag161619@gmail.com>
2024-09-11 21:24:39 +0530

    simple router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import {Button } from './components/ui/button';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Button>Click me</Button>
+    <Outlet />
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,34 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
+import Home from './Home/index.jsx'
+import Dashboard from './Dashboard/index.jsx'
+
+
+const router=createBrowserRouter([
+  {
+    element:<App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard/>,
+      },
+    ]
+  },
+  {
+    path: "/auth/sign-in",
+    element: <SignInPage/>,
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
